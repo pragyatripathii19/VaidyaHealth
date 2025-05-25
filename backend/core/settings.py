d6632384356632473537f03b777c331c78a55cc0
AK4CODER <akshitnair2004@gmail.com>
2025-05-26 00:06:13 +0530

    updated settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-63shicn!8(+3=5wiom@ku!)ep^uixv(%50@&feaf9&_$@2b+@)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vaidyahealth-1.onrender.com', 'localhost','127.0.0.1']
 
 
 # Application definition
