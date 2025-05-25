41a61b43d5ceb4ed714d3931e06ee07c22c48781
AK4CODER <akshitnair2004@gmail.com>
2025-05-25 22:55:14 +0530

    predictor to backend

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -3,7 +3,7 @@ import sys
 
 # Replace with your Django project name
 sys.path.append(os.path.dirname(_file_))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "predictor.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
